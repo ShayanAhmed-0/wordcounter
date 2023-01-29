61dd9352774ac3617664f714bbc43a3c477d9481
Shayan <shayanlost1@gmail.com>
2023-01-29 17:31:13 +0500

    word counter

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+
+let count:number;
+let vari:string;
+let val:string;
+let flag:boolean=true;
+const func=async()=>{
+        const fun=await inquirer.prompt({
+            name:"name",
+            type:"list",
+            message:"Do you Want to Countinue",
+            choices: ["Countinue","Exit"]
+        })
+        if(fun.name=="Countinue"){
+            flag=true;
+        }else{
+            flag=false;
+        }
+    }
+
+    const takeVal=async()=>{
+        const Val=await inquirer.prompt([
+             {
+            name:"words",
+            type:"string",
+            message:"Enter words: ",
+        },
+    ])
+        val =Val.words;
+        console.log("total Number of words including spaces: "+val.length);
+        let newVal:string[]=val.split(" ");
+        let withoutspaces:number=val.length-newVal.length+1;
+        console.log("total Number of words without including spaces: "+withoutspaces);
+    }    
+while(flag){
+    await takeVal();
+    await func();
+}    
